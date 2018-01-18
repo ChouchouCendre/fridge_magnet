5da241ead85af0561c01e57ea9fadf497452a33c
Sun <sunyi.limet@digitaslbi.fr>
2018-01-18 17:43:05 +0100

    Tesseract

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+/* global Tesseract */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Tesseract from 'tesseract.js';
 
 import './scss/index.scss';
 
@@ -9,26 +10,67 @@ class FridgeMagnet extends React.Component {
   constructor(props) {
     super(props);
 
-    /*
     this.state = {
-      tarif: cTarif || 3.70, // Tarif horaire NET
+      words: ['plop'],
     };
-    */
 
+    this.changeInputFile = this.changeInputFile.bind(this);
     this.recognizeFile = this.recognizeFile.bind(this);
   }
 
-  recognizeFile(e) {
-    console.log('@@ recognizeFile', e);
-    // window.lastFile=this.files[0]
-    console.log('window.lastFile', window.lastFile);
+  changeInputFile(files) {
+    console.log('@@ changeInputFile', files);
+
+    const file = files[0];
+    /*
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      this.setState({
+        file,
+        imagePreviewUrl: reader.result,
+      });
+    };
+
+    reader.readAsDataURL(file);
+    */
+
+    // <input type="file" onchange="Tesseract.recognize(this.files[0]).progress(function(data){console.log(data)}).then(function(data){console.log(data)})">
+
+    // console.log('file', file);
+    Tesseract.recognize(file, { lang: 'fra', classify_character_fragments_garbage_certainty_threshold: -30 })
+      .progress(message => console.log(message))
+      .catch(err => console.error(err))
+      .then(result => console.log(result))
+      .finally(resultOrError => this.recognizeFile(resultOrError));
+  }
+
+  recognizeFile(resultOrError) {
+    console.log('resultOrError', resultOrError);
+    // resultOrError.words.map(value => );
+    this.setState({ words: resultOrError.lines });
+  }
+
+  renderWords() {
+    /*
+    return (
+      <div className="container-content_cards">
+        { this.state.questions.map((question, index) => this.injectCard(question, index)) }
+      </div>
+    );
+    */
+    console.log('this.state.words', this.state.words);
+    return this.state.words.map(item => `${item.text}---`);
   }
 
   render() {
-
     return (
       <div className="container">
-        <input type="file" onChange={ this.recognizeFile } />
+        <input type="file" onChange={ e => this.changeInputFile(e.target.files) } />
+        {/*
+        <img src={this.state.imagePreviewUrl} />
+        */}
+        { this.renderWords() }
       </div>
     );
   }
